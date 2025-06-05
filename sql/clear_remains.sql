dca54264f5763e9561352cc3f140befff2e38ee0
uldis <uldis@weberp.lv>
2025-06-05 16:21:18 +0300

    Add SQL script to clear remains from D4Store tables

---
+++ sql/clear_remains.sql
@@ -0,0 +1,6 @@
+DELETE FROM `d4store_action_ref`;
+DELETE FROM `d4store_action`;
+DELETE FROM `d4store_pack_product_history`;
+DELETE FROM `d4store_pack_product`;
+DELETE FROM `d4store_packs`;
+DELETE FROM `d4store_store_product`;
